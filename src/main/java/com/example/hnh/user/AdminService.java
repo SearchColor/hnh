9ff75489cebbc3eda9caa9019e2528cc852ae208
박재혁 <jaeh3197@naver.com>
2025-01-07 17:25:38 +0900

    feat: 관리자 생성 기능 구현 (#2)
    * feat: S3config 추가
    
    build.gradle 에 aws 를 추가하고 properties 에 환경변수를 추가하였습니다
    global 에 config 를 추가하여 S3config 를 등록하였습니다.
    
    * feat: 이미지 업로드 기능 구현
    
    이미지를 업로드하는 기능을 구현하였습니다.
    
    * feat: 관리자 생성 기능 구현
    
    관리자 생성 기능을 구현하였습니다.

---
+++ src/main/java/com/example/hnh/user/AdminService.java
@@ -0,0 +1,42 @@
+package com.example.hnh.user;
+
+import com.example.hnh.global.error.errorcode.ErrorCode;
+import com.example.hnh.global.error.exception.CustomException;
+import com.example.hnh.user.dto.AdminCreateRequestDto;
+import com.example.hnh.user.dto.AdminResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class AdminService {
+
+    private final UserRepository userRepository;
+
+    /**
+     * 관리자 생성 로직
+     *
+     * @param adminCreateRequestDto 관리자 생성 정보
+     * @return
+     */
+    @Transactional
+    public AdminResponseDto createAdmin(AdminCreateRequestDto adminCreateRequestDto) {
+
+        //이메일 중복 검증
+        Optional<User> findUser = userRepository.findByEmail(adminCreateRequestDto.getEmail());
+
+        if (findUser.isPresent()) {
+            throw new CustomException(ErrorCode.DUPLICATE_RESOURCE);
+        }
+
+        //관리자 객체 생성, 저장
+        User admin = adminCreateRequestDto.toEntity();
+        admin.updateAuth("admin");
+        User savedAdmin = userRepository.save(admin);
+
+        return new AdminResponseDto(savedAdmin);
+    }
+}
