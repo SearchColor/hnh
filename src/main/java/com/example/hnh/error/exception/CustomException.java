c01aa8954325ce175d0f80e7d1e9c85144ae974a
searchcolor <qkrdydwo1022@gmail.com>
2025-01-07 10:49:41 +0900

    Add : package 생성

---
+++ src/main/java/com/example/hnh/error/exception/CustomException.java
@@ -0,0 +1,11 @@
+package com.example.hnh.error.exception;
+
+import com.example.hnh.error.errorcode.ErrorCode;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CustomException extends RuntimeException {
+    private final ErrorCode errorCode;
+}
