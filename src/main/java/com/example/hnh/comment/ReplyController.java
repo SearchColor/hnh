fb07266f825e63c84476f648acc75db02d98d088
Dawnfeeling <swhan98@naver.com>
2025-01-09 14:46:33 +0900

    feat : 답글 삭제 기능 추가
    - 답글 삭제 API

--- src/main/java/com/example/hnh/comment/ReplyController.java
+++ src/main/java/com/example/hnh/comment/ReplyController.java
@@ -2,11 +2,13 @@ package com.example.hnh.comment;
 
 import com.example.hnh.comment.dto.ReplyRequestDto;
 import com.example.hnh.comment.dto.ReplyResponseDto;
+import com.example.hnh.global.MessageResponseDto;
 import com.example.hnh.global.config.auth.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -43,6 +45,18 @@ public class ReplyController {
         Long userId = userDetails.getUser().getId();
 
         ReplyResponseDto replyResponseDto = replyService.updateReply(userId, replyId, dto.getReply());
+
+        return new ResponseEntity<>(replyResponseDto, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{replyId}")
+    public ResponseEntity<MessageResponseDto> deleteReply(
+            @PathVariable Long replyId,
+            @AuthenticationPrincipal UserDetailsImpl userDetails) {
+
+        Long userId = userDetails.getUser().getId();
+
+        MessageResponseDto replyResponseDto = replyService.deleteReply(userId, replyId);
 
         return new ResponseEntity<>(replyResponseDto, HttpStatus.OK);
     }
