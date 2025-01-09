2283a3d7951c4a9b6ea457b130f6c8349bdf949b
SeungWan <95282509+Dawnfeeling@users.noreply.github.com>
2025-01-09 13:59:48 +0900

    feat : 댓글 생성 기능 추가
    - 댓글 생성 API

---
+++ src/main/java/com/example/hnh/comment/CommentController.java
@@ -0,0 +1,33 @@
+package com.example.hnh.comment;
+
+import com.example.hnh.comment.dto.CommentRequestDto;
+import com.example.hnh.comment.dto.CommentResponseDto;
+import com.example.hnh.global.config.auth.UserDetailsImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/comment")
+public class CommentController {
+
+    private final CommentService commentService;
+
+    @PostMapping
+    public ResponseEntity<CommentResponseDto> addComment(
+            @RequestBody CommentRequestDto dto,
+            @AuthenticationPrincipal UserDetailsImpl userDetails) {
+
+        Long userId = userDetails.getUser().getId();
+
+        CommentResponseDto commentResponseDto = commentService.addComment(userId, dto.getBoardId(), dto.getComment());
+
+        return new ResponseEntity<>(commentResponseDto, HttpStatus.CREATED);
+    }
+}
