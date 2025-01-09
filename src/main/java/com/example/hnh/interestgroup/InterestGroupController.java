fb17662361ec2c86910d880b0986e0212bc60e7c
myoma <qq8380qq@gmail.com>
2025-01-09 11:40:49 +0900

    feat : 관심 그룹 찜하기 API 기능 구현

--- src/main/java/com/example/hnh/interestgroup/InterestGroupController.java
+++ src/main/java/com/example/hnh/interestgroup/InterestGroupController.java
@@ -1,4 +1,38 @@
 package com.example.hnh.interestgroup;
 
+import com.example.hnh.global.config.auth.UserDetailsImpl;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/groups")
 public class InterestGroupController {
+
+    private final InterestGroupService interestGroupService;
+
+    public InterestGroupController(InterestGroupService interestGroupService) {
+        this.interestGroupService = interestGroupService;
+    }
+
+    /**
+     * 관심 그룹 찜하기 API
+     * @param groupId
+     * @param userDetails
+     * @return
+     */
+    @PostMapping("/{groupId}/interest")
+    public ResponseEntity<String> toggleInterest(
+            @PathVariable Long groupId,
+            @AuthenticationPrincipal UserDetailsImpl userDetails) {
+
+        Long userId = userDetails.getUser().getId();
+        String message = interestGroupService.toggleInterest(groupId, userId);
+
+        return ResponseEntity.ok(message);
+    }
 }
+
