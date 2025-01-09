340b6c36a8503cb8de43eeb86b50881cd49b46e6
박재혁 <jaeh3197@naver.com>
2025-01-09 11:01:41 +0900

    feat: 그룹 리포트 기능 구현
    * feat: 그룹 리포트 기능 구현
    
    관리자가 해당 그룹을 리포트하는 기능을 구현하였습니다.

--- src/main/java/com/example/hnh/user/AdminService.java
+++ src/main/java/com/example/hnh/user/AdminService.java
@@ -2,10 +2,9 @@ package com.example.hnh.user;
 
 import com.example.hnh.global.error.errorcode.ErrorCode;
 import com.example.hnh.global.error.exception.CustomException;
-import com.example.hnh.user.dto.AdminCreateRequestDto;
-import com.example.hnh.user.dto.AdminResponseDto;
-import com.example.hnh.user.dto.ReportUserRequestDto;
-import com.example.hnh.user.dto.ReportUserResponseDto;
+import com.example.hnh.group.Group;
+import com.example.hnh.group.GroupRepository;
+import com.example.hnh.user.dto.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -19,6 +18,7 @@ import java.util.Optional;
 public class AdminService {
 
     private final UserRepository userRepository;
+    private final GroupRepository groupRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     /**
@@ -69,5 +69,29 @@ public class AdminService {
         User savedUser = userRepository.save(findUser);
 
         return new ReportUserResponseDto(savedUser.getId(), savedUser.getStatus());
+    }
+
+    /**
+     * 그룹 리포트 로직
+     *
+     * @param reportGroupRequestDto 그룹 리포트 정보
+     * @return
+     */
+    public ReportGroupResponseDto reportGroup(ReportGroupRequestDto reportGroupRequestDto) {
+
+        //그룹 조회
+        Group findGroup = groupRepository.findById(reportGroupRequestDto.getGroupId()).orElseThrow(
+                () -> new CustomException(ErrorCode.GROUP_NOT_FOUND));
+
+        //요청 값과 같은 값인지 검증
+        if (Objects.equals(findGroup.getStatus(), reportGroupRequestDto.getStatus())) {
+            throw new CustomException(ErrorCode.DUPLICATE_RESOURCE);
+        }
+
+        //그룹 상태 변경, 저장
+        findGroup.setStatus(reportGroupRequestDto.getStatus());
+        Group savedGroup = groupRepository.save(findGroup);
+
+        return new ReportGroupResponseDto(savedGroup.getId(), savedGroup.getStatus());
     }
 }
