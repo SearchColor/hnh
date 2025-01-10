4c43aeda47bc4d190af856dc6c7f3f2e5b468376
myoma <qq8380qq@gmail.com>
2025-01-10 14:03:12 +0900

    add : 3-layered 파일 생성

---
+++ src/main/java/com/example/hnh/meet/MeetController.java
@@ -0,0 +1,9 @@
+package com.example.hnh.meet;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/groups")
+public class MeetController {
+}
---
+++ src/main/java/com/example/hnh/meet/MeetService.java
@@ -0,0 +1,7 @@
+package com.example.hnh.meet;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MeetService {
+}
