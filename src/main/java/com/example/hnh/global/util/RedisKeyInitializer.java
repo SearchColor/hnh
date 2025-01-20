8b29639d5afa87bf1a41dd5d2ef0c7da5dd23076
SeungWan <95282509+Dawnfeeling@users.noreply.github.com>
2025-01-20 17:48:27 +0900

    refactor : redis key 관리 수정
    - 애플리케이션 재시작 시 특정 redis key가 초기화 되도록 수정

--- src/main/java/com/example/hnh/global/util/RedisKeyInitializer.java
+++ src/main/java/com/example/hnh/global/util/RedisKeyInitializer.java
@@ -1,20 +1,31 @@
 package com.example.hnh.global.util;
 
 import jakarta.annotation.PostConstruct;
+import org.springframework.core.env.Environment;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
+
+import java.util.Set;
 
 @Component
 public class RedisKeyInitializer {
 
     private final RedisTemplate<String, String> redisTemplate;
+    private final Environment environment;
 
-    public RedisKeyInitializer(RedisTemplate<String, String> redisTemplate) {
+    public RedisKeyInitializer(RedisTemplate<String, String> redisTemplate, Environment environment) {
         this.redisTemplate = redisTemplate;
+        this.environment = environment;
     }
 
     @PostConstruct
     public void clearKeys() {
-        redisTemplate.delete(redisTemplate.keys("board:view:*"));
+        String ddlAuto = environment.getProperty("spring.jpa.hibernate.ddl-auto");
+        if("create".equalsIgnoreCase(ddlAuto) || "create-drop".equalsIgnoreCase(ddlAuto)) {
+            Set<String> keys = redisTemplate.keys("board:view:*");
+            if(keys != null && !keys.isEmpty()) {
+                redisTemplate.delete(keys);
+            }
+        }
     }
 }
